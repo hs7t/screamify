67b575b7bb38faf6a9b198a333178ce2c8e20d7a
Alí Sinisterra <haidarconh@gmail.com>
2025-09-25 13:59:53 -0500

    move link logic away from main app screen

---
+++ src/links.js
@@ -0,0 +1,63 @@
+const characterNames = new Map([
+  [".", "dot"],
+  [":", "colon"],
+  [",", "comma"],
+  ["/", "slash"],
+  ["\\", "backslash"],
+  ["$", "currencysign"],
+  ["@", "at"],
+  ["?", "questionmark"],
+  ["(", "openparenthesis"],
+  [")", "closeparenthesis"],
+  ["=", "equals"],
+  ["&", "ampersand"],
+  [" ", "space"],
+]);
+
+const lengthenWithSymbolRep = (char) => {
+  let symbolRepresentation;
+  if (characterNames.has(char)) {
+    symbolRepresentation = characterNames.get(char);
+  } else {
+    symbolRepresentation = char;
+  }
+
+  return symbolRepresentation;
+};
+
+const lengthenWithASCII = (char) => {
+  const characterCode = char.charCodeAt(0);
+  let characterRepresentation;
+
+  const divider = 20;
+
+  const aAmount = (characterCode - (characterCode % divider)) / divider;
+  const hAmount = characterCode % divider;
+
+  characterRepresentation = "A".repeat(aAmount) + "H".repeat(hAmount);
+  return characterRepresentation;
+};
+
+const getLengthenedCharacter = (char) => {
+  let output = [];
+
+  if (/^[a-zA-Z]*$/.test(char)) {
+    output.push(lengthenWithASCII(char));
+  } else {
+    output.push(lengthenWithSymbolRep(char));
+  }
+
+  return output;
+};
+
+const lengthenLink = (link) => {
+  let textBits = [];
+  for (const char of link) {
+    textBits.push(getLengthenedCharacter(char));
+  }
+
+  let longLink = textBits.join(".");
+  return longLink;
+};
+
+export { lengthenLink };
