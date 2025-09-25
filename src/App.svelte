cbb6182bc9f7fae3fee34f329b16f4b831fe56b5
Alí Sinisterra <haidarconh@gmail.com>
2025-09-25 14:03:47 -0500

    add LinkCreator

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,8 @@
 <script>
   import { get } from "svelte/store";
-  import { lengthenLink } from "./links";
-
-  let link = $state("")
-
-  let lengthenedLink = $derived(lengthenLink(link))
-  let currentLocation = window.location.pathname
+  import LinkCreator from "./lib/LinkCreator.svelte";
 </script>
 
 <main>
-  <input type="text" bind:value={link}>
-  <p>{link}</p>
-  <p>{lengthenedLink}</p>
-  <p>{currentLocation}</p>
+  <LinkCreator />
 </main>
---
+++ src/lib/LinkCreator.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { get } from "svelte/store";
+  import { lengthenLink } from "../links";
+
+  let link = $state("")
+
+  let lengthenedLink = $derived(lengthenLink(link))
+  let currentLocation = window.location.pathname
+</script>
+
+<div>
+  <h1>Link too short?</h1>
+  <label for="linkInput">Type in a link:</label>
+
+  <input type="text" bind:value={link} id="linkInput">
+  <p>{link}</p>
+  <p>{lengthenedLink}</p>
+  <p>{currentLocation}</p>
+</div>
